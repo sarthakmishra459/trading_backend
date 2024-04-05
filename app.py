86650a61c6045893c330be5f97a70f5d5c0f5e76
Sarthak Mishra <124512392+sarthakmishra459@users.noreply.github.com>
2024-04-05 09:46:48 +0530

    "Trading app Api"

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import numpy as np
 import pickle
 import tensorflow as tf
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Body
 app = FastAPI()
-
+from sklearn.preprocessing import MinMaxScaler
+import uvicorn
 # Load the model and scaler from the pickle files
 MODEL = tf.keras.models.load_model('model/')
 
@@ -13,15 +15,11 @@ with open('scaler.pkl', 'rb') as f:
     scaler = pickle.load(f)
     
 
-origins = [
-    "https://trading-backend-vyzs.onrender.com",  # replace with the origin of your frontend
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],  # Update this with your frontend URL
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["POST"],
     allow_headers=["*"],
 )
 
@@ -29,11 +27,16 @@ class InputData(BaseModel):
     data: list
 
 @app.post("/predict/")
-async def predict(data: InputData):
-    input_array = np.array(data.data)
+async def predict(data: InputData = Body):
+    print("Data Received",data)
+    input_array = np.array(data.data[0])
+    print("Data Received",input_array)
+    
+    scaled_input = scaler.fit_transform(input_array.reshape(-1, 1))
+    print("Scaled Input",scaled_input)
     n_steps = 100
-    temp_input = input_array.tolist()  # Initialize temp_input with the input data provided by the user
-
+    temp_input = scaled_input.flatten().tolist()  
+    print("Temp Input",temp_input)
     lst_output = []
     i = 0
     while i < 30:
@@ -54,5 +57,14 @@ async def predict(data: InputData):
             lst_output.extend(yhat.tolist())
             i += 1
     # Inverse transform the predicted values before returning them
+    print("output",lst_output)
     response = scaler.inverse_transform(np.array(lst_output)).flatten().tolist()
+    print("response",response)
     return {"prediction": response}
+if(__name__) == '__main__':
+        uvicorn.run(
+        "app:app",
+        host    = "0.0.0.0",
+        port    = 10000, 
+        reload  = True
+    )
